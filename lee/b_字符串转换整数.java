14334dacd7600ccf51f0223e5562f361241532b9
liu <a2506991786@qq.com>
2023-11-12 19:49:54 +0800

    字符串转换整数

---
+++ lee/b_字符串转换整数.java
@@ -0,0 +1,54 @@
+public class b_字符串转换整数 {
+    public static void main(String[] args) {
+           myAtoi(" ");
+    }
+
+    /**
+     * 请你来实现一个 myAtoi(string s) 函数，使其能将字符串转换成一个 32 位有符号整数（类似 C/C++ 中的 atoi 函数）。
+     * 函数 myAtoi(string s) 的算法如下：
+     * 读入字符串并丢弃无用的前导空格
+     * 检查下一个字符（假设还未到字符末尾）为正还是负号，读取该字符（如果有）。 确定最终结果是负数还是正数。 如果两者都不存在，则假定结果为正。
+     * 读入下一个字符，直到到达下一个非数字字符或到达输入的结尾。字符串的其余部分将被忽略。
+     * 将前面步骤读入的这些数字转换为整数（即，"123" -> 123， "0032" -> 32）。如果没有读入数字，则整数为 0 。必要时更改符号（从步骤 2 开始）。
+     * 如果整数数超过 32 位有符号整数范围 [−231,  231 − 1] ，需要截断这个整数，使其保持在这个范围内。具体来说，小于 −231 的整数应该被固定为 −231 ，大于 231 − 1 的整数应该被固定为 231 − 1 。
+     * 返回整数作为最终结果。
+     * my atoi
+     *无聊的题目
+     * @param s s
+     * @return int
+     */
+    public static int myAtoi(String s) {
+        int index = 0,isPositive=1,len = s.length();
+        long i =0;
+        char c;
+        if(s.isEmpty()){
+          return 0;
+        }
+        while (index<s.length() &&s.charAt(index)==' '){
+               index++;
+        }
+        if(index == s.length()){
+            return 0;
+        }
+        c = s.charAt(index);
+        if (c == '-' || c == '+') {
+            if(c == '-') {
+                isPositive = -1;
+            }
+            index++;
+        }
+        while (index<len && s.charAt(index) <= 57 && s.charAt(index)>=48
+        ){
+            i*=10;
+            i += s.charAt(index) - 48;
+            if(isPositive*i > 2147483647 || isPositive*i < -2147483648){
+                if(isPositive*i>0) {
+                    return 2147483647;
+                }
+                return -2147483648;
+            }
+            index++;
+        }
+        return (int)i*isPositive;
+    }
+}
