fdd8f743b3fe9408a885f72c434c0088fd39bbf3
liu <a2506991786@qq.com>
2023-11-18 01:40:04 +0800

    dp

---
+++ lee/src/main/java/org/example/lee/dp/c_最长有效括号.java
@@ -0,0 +1,42 @@
+package org.example.lee.dp;
+
+/**
+ * c 最长有效括号
+ *
+ * @author 刘家辉
+ * @date 2023/11/18
+ */
+public class c_最长有效括号 {
+
+
+    /**
+     * 给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+     * longest valid parentheses
+     * 动态规划
+     * TODO     思路：动态规划
+     *     1. 状态定义：dp[i]表示以s[i]结尾的最长有效括号子串的长度
+     *     2. 状态转移方程：
+     *       1. s[i] == '('，dp[i] = 0
+     *       2. s[i] == ')'，s[i-1] == '('，dp[i] = dp[i-2](边界: i-2>=0) + 2
+     *       3. s[i] == ')'，s[i-1] == ')'，s[i-dp[i-1]-1](边界: i-dp[i-1]-1>=0) == '('，dp[i] = dp[i-1] + 2 + dp[i-dp[i-1]-2](边界: i-dp[i-1]-2>=0)
+     *     3. 初始状态：dp[0] = 0
+     *     4. 返回值：max(dp[i])
+     * @param s s
+     * @return int
+     */
+    public int longestValidParentheses(String s) {
+         int max =0,len = s.length();
+         int dp[] = new int[len];
+        for (int i = 1; i < len; i++) {
+            if(s.charAt(i) == ')'){
+                if(s.charAt(i-1) == '('){
+                    dp[i] = (i>=2?dp[i-2]:0)+2;
+                } else if (i-dp[i-1]>0&&s.charAt(i-dp[i-1]-1) == '(') {
+                    dp[i] = dp[i-1] + ((i-dp[i-1]) >= 2?dp[i-dp[i-1]-2]:0)+2;
+                }
+                max = Math.max(max,dp[i]);
+            }
+        }
+        return max;
+    }
+}
