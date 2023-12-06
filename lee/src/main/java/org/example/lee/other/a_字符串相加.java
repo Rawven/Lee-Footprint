96944eaa845bb09a7bfe1c15351616a266023489
liu <a2506991786@qq.com>
2023-12-06 19:45:37 +0800

    12-06 字符串相加相乘

---
+++ lee/src/main/java/org/example/lee/other/a_字符串相加.java
@@ -0,0 +1,34 @@
+package org.example.lee.other;
+
+public class a_字符串相加 {
+
+
+    /**
+     * add strings
+     * 如题 不准用 BigInteger之类的
+     * @param num1 num1
+     * @param num2 num2
+     * @return {@link String}
+     */
+    public String addStrings(String num1, String num2) {
+        int len1 = num1.length(),len2 = num2.length();
+        int tool = '0',tool2=0,save = 0;
+        StringBuilder sb = new StringBuilder();
+        while (len1>0 || len2>0||save>0){
+            if(len1>0) {
+                tool2+=(num1.charAt(len1-1)-tool);
+                len1--;
+            }
+            if(len2>0){
+                tool2+=(num2.charAt(len2-1)-tool);
+                len2--;
+            }
+            tool2+=save;
+            sb.append((char)(tool2%10+'0'));
+            save = tool2/10;
+            tool2=0;
+        }
+        return sb.reverse().toString();
+    }
+
+}
---
+++ lee/src/main/java/org/example/lee/滑动窗口/b_最大点数.java
@@ -0,0 +1,35 @@
+package org.example.lee.滑动窗口;
+
+public class b_最大点数 {
+
+    /**
+     * 几张卡牌 排成一行，每张卡牌都有一个对应的点数。点数由整数数组 cardPoints 给出。
+     * 每次行动，你可以从行的开头或者末尾拿一张卡牌，最终你必须正好拿 k 张卡牌。
+     * 你的点数就是你拿到手中的所有卡牌的点数之和。
+     * 给你一个整数数组 cardPoints 和整数 k，请你返回可以获得的最大点数。
+     * max score
+     * TODO 关键思想  可获得的最大点数 = 总数-滑动窗口的min
+     * @param cardPoints card points
+     * @param k          k
+     * @return int
+     */
+    public int maxScore(int[] cardPoints, int k) {
+        int left=0,right=0,min=0,record=0,all=0,len = cardPoints.length;
+        while (right<len){
+            min+=cardPoints[right];
+            all+=cardPoints[right];
+            right++;
+            if(right>len-k){
+                min-=cardPoints[left];
+                left++;
+                if(min<record){
+                    record=min;
+                }
+            }else{
+                record=min;
+            }
+
+        }
+        return all-record;
+    }
+}
