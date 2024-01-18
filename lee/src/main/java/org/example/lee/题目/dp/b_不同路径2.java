1f95a02907f8fe78d4e525cb9a9a4a1491c12f12
liu <a2506991786@qq.com>
2024-01-19 00:01:07 +0800

    1.18

---
+++ lee/src/main/java/org/example/lee/题目/dp/b_不同路径2.java
@@ -0,0 +1,30 @@
+package org.example.lee.题目.dp;
+
+public class b_不同路径2 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        int mS, nS;
+        mS = nS = 1 - obstacleGrid[m - 1][n - 1];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (obstacleGrid[i][j] != 1) {
+                    if (i == m - 1) {
+                        dp[i][j] = mS;
+                    } else if (j == n - 1) {
+                        dp[i][j] = nS;
+                    } else {
+                        dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+                    }
+                } else {
+                        mS = i==m-1?0:1;
+                        nS = j == n - 1?0:1;
+                }
+            }
+        }
+        return dp[0][0];
+
+    }
+
+
+}
---
+++ lee/src/main/java/org/example/lee/题目/dp/b_最小路径和.java
@@ -0,0 +1,27 @@
+package org.example.lee.题目.dp;
+
+public class b_最小路径和 {
+
+    /**
+     *  dp[i]
+     * @param obstacleGrid
+     * @return
+     */
+    public int minPathSum(int[][] obstacleGrid) {
+        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+        for (int i = 0; i <obstacleGrid.length ; i++) {
+            for (int j = 0; j <obstacleGrid[0].length ; j++) {
+                if(i == 0 && j == 0) {
+                    dp[0][0]= obstacleGrid[i][j];
+                }else if(i ==0){
+                    dp[i][j] = obstacleGrid[i][j] + dp[i][j-1];
+                }else if(j == 0){
+                    dp[i][j] = obstacleGrid[i][j] + dp[i-1][j];
+                }else {
+                    dp[i][j] = obstacleGrid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[obstacleGrid.length-1][obstacleGrid[0].length-1];
+    }
+}
