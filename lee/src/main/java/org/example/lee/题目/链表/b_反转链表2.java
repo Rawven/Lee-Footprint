27c3eeb3334f9a3557d15d3ab417c1c731cd1b0d
liu <a2506991786@qq.com>
2024-01-26 02:05:59 +0800

    1.25

---
+++ lee/src/main/java/org/example/lee/题目/链表/b_反转链表2.java
@@ -0,0 +1,47 @@
+package org.example.lee.题目.链表;
+
+import org.example.lee.model.ListNode;
+import org.example.lee.model.ListNodeUtil;
+
+public class b_反转链表2 {
+
+
+    /**
+     * reverse between
+     * 反转链表+头尾处理
+     * @param head  head
+     * @param left  left
+     * @param right right
+     * @return {@link ListNode}
+     */
+    public  ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || head.next == null || left >= right) {
+            return head;
+        }
+        ListNode realH = head, l, f, s = null, h, e = null;
+        int i = 1;
+        while (i < left) {
+            s = head;
+            head = head.next;
+            i++;
+        }
+        l = s;
+        f = head;
+        while (i < right) {
+            h = head.next;
+            head.next = e;
+            e = head;
+            head = h;
+            i++;
+        }
+        f.next = head.next;
+        head.next = e;
+        if (l != null) {
+            l.next = head;
+            return realH;
+        }
+        return head;
+    }
+
+
+}
