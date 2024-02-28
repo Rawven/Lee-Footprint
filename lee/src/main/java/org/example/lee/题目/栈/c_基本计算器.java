1bf8801bddbeb97c52d3f1a6e57fe19512032c5d
liu <a2506991786@qq.com>
2024-02-28 21:49:23 +0800

    2.28 计算器 我写牛魔

---
+++ lee/src/main/java/org/example/lee/题目/栈/c_基本计算器.java
@@ -0,0 +1,45 @@
+package org.example.lee.题目.栈;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Stack;
+
+public class c_基本计算器 {
+
+
+    public  int calculate(String s){
+        Stack<Integer> stack = new Stack<>();
+        int i=0,anw=0,sign=1;
+        long save;
+        //初始符号为正
+        stack.push(1);
+        while (i<s.length()){
+            if(s.charAt(i)=='+'){
+                //同当前源符号
+                sign= stack.peek();
+            }else if(s.charAt(i)=='-'){
+                //同当前源符号的反
+                sign=-stack.peek();
+            }else if(s.charAt(i)=='('){
+                //记录括号前的作为当前源符号
+                stack.push(sign);
+            }else if(s.charAt(i)==')'){
+                //离开当前括号
+                stack.pop();
+            }else if(!(s.charAt(i)==' ')){
+                //遭遇数字
+                save=0;
+                while (i<s.length()&&Character.isDigit(s.charAt(i))){
+                    save=save*10+ s.charAt(i)-'0';
+                    i++;
+                }
+                i--;
+                anw+= (int) (sign*save);
+            }
+            i++;
+        }
+        return anw;
+    }
+
+}
