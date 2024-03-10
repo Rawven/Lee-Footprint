d291e4164e1d842fcda5f487a9e1f648f890ba22
liu <a2506991786@qq.com>
2024-03-10 16:30:54 +0800

    3.10 dp

--- lee/src/main/java/org/example/lee/Test.java
+++ lee/src/main/java/org/example/lee/Test.java
@@ -1,6 +1,10 @@
 package org.example.lee;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * test
@@ -8,9 +12,10 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author 刘家辉
  * @date 2024/01/13
  */
+@SpringBootApplication
 public class Test {
     public static void main(String[] args) {
-        ConcurrentHashMap<Object, Object> map = new ConcurrentHashMap<>();
+        SpringApplication.run(Test.class, args);
 
     }
 }
---
+++ lee/src/main/java/org/example/lee/题目/其它/a_加一.java
@@ -0,0 +1,27 @@
+package org.example.lee.题目.其它;
+
+public class a_加一 {
+    public int[] plusOne(int[] digits) {
+        int i = digits.length-1;
+        int a = ++digits[i];
+        if(a !=10){
+            return digits;
+        }
+        while(a == 10){
+            digits[i]=0;
+            i--;
+            if(i<0){
+                break;
+            }
+            digits[i]++;
+            a = digits[i];
+        }
+        if(i<0){
+            //因为只有一种特殊情况
+            int[] aaa = new int[digits.length+1];
+            aaa[0]=1;
+            return aaa;
+        }
+        return digits;
+    }
+}
