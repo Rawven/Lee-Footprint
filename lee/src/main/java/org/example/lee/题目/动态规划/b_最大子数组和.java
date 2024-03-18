94c19b65b53934c791b6bab1ee84be32b598e7d9
liu <a2506991786@qq.com>
2024-03-18 15:20:59 +0800

    3.18 dp搞搞

---
+++ lee/src/main/java/org/example/lee/题目/动态规划/b_最大子数组和.java
@@ -0,0 +1,16 @@
+package org.example.lee.题目.动态规划;
+
+public class b_最大子数组和 {
+    public int maxSubArray(int[] nums) {
+        int h=0, max = Integer.MIN_VALUE;
+        for (int k : nums) {
+            h += k;
+            max = Math.max(max, h);
+            if (h < 0) {
+                h = 0;
+            }
+        }
+        return max;
+    }
+    
+}
